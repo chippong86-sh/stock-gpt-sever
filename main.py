f58a3b1319f770ed96ee0793731bc06e7f1876cd
chippong-sh <chippong86@gmail.com>
2026-04-10 23:07:47 +0900

    add analyze-stock endpoint

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+@app.get("/analyze-stock")
+def analyze_stock(query: str):
+    return {
+        "status": "ok",
+        "종목": query,
+        "메시지": "API 연결 테스트 성공"
+    }
